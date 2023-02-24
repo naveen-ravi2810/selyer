eedc0b3dafcc4d1331d45a0b9aae8c3ac0b2bef7
Naveen Ravi Chandran <rnaveen28102003@gmail.com>
2023-02-24 18:27:41 +0530

    updated

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import base64
 from validate_email import validate_email
 from dotenv import load_dotenv
 import os
+import bcrypt
 
 load_dotenv()
 
@@ -54,8 +55,8 @@ def login():
         details = request.form
         name = details['username']
         password = details['password']
-        user = users_column.find_one({"$or":[{'Email':name,'Password':password},{'Phone':name,'Password':password}]})
-        if user:
+        user = users_column.find_one({"$or":[{'Email':name},{'Phone':name}]})
+        if bcrypt.checkpw(password.encode('utf-8'), user['Password']):
             session['name'] = user['First_Name']
             session['role'] = user['Role']
             session['email'] = user['Email']
@@ -75,10 +76,11 @@ def signup():
         email = details['email_address']
         phone = details['phone']
         password = details['password']
+        password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         gender = details['gender']
         otp=  details['otp']
         if int(otp)==session['new_otp']  :
-            b= add_user(first_name, last_name, email, phone, password, gender)
+            b= add_user(first_name, last_name, email, phone, password_hash, gender)
             return redirect('/login')
         else:
             msg = "Wrong Otp"
@@ -95,6 +97,44 @@ def generate_otp_email():
         else :
             otp_status = email_otp
     return jsonify ({'success':True,'otp':otp_status})
+
+@app.route('/forgot_password',methods=['GET','POST'])
+def forgot_password():
+    msg=" "
+    if request.method == 'POST':
+        email=request.form['email']
+        user = users_column.find_one({'Email':email})
+        if user:
+            email_otp = generate_otp(email)
+            session['forgot_password_email'] = email
+            session['forgot_password_otp'] = email_otp
+            return redirect('/forgot_password_otp')
+        else :
+            msg="The Email Dont Exist"
+    return render_template('forgot_password.html', msg=msg)
+
+@app.route('/forgot_password_otp',methods=['GET','POST'])
+def forgot_password_otp():
+    if request.method == 'POST':
+        entered_otp = request.form['otp']
+        email_otp = session['forgot_password_otp']
+        if int(entered_otp) == email_otp:
+            return redirect('/reset_password')
+    return render_template('forgot_password_otp.html')
+
+@app.route('/reset_password',methods=['GET','POST'])
+def reset_password():
+    msg = " "
+    if request.method == 'POST':
+        new_password = request.form['new_password']
+        reenter_password = request.form['reenter_password']
+        if new_password == reenter_password:
+            password_hash = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+            users_column.update_one({'Email':session['forgot_password_email']}, { "$set": { "Password": password_hash } })
+            return redirect('/login')
+        else:
+            msg = "Passwords didn't match"
+    return render_template('reset_password.html',msg=msg)
 
 @app.route("/dashboard",methods=['GET','POST'])
 def dashboard():
